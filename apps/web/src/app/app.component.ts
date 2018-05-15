73dcf50d0578c3314349b09b29a4a41e00ee79f8
Val Neekman <val@neekware.com>
2018-05-15 17:07:22 -0400

    format

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CfgService, DefaultCfg } from 'pkgs/cfg';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '@nwx/cfg';
+  options = {};
+  constructor(public cfg: CfgService) {
+    this.options = { ...DefaultCfg, ...{ pkgName: this.title } };
+  }
 }
