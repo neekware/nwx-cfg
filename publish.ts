ce75cfd753f6d81fb7d05e1099125c6bb20eaff0
Val Neekman <val@neekware.com>
2018-05-15 18:02:21 -0400

    publish file

---
+++ publish.ts
@@ -0,0 +1,30 @@
+import { exec } from 'child_process';
+
+const jsonPkg = require('./package.json');
+const version = jsonPkg.version;
+const buildPath = 'builds/nwx-cfg';
+const publishOptions = '--access public --non-interactive --no-git-tag-version';
+
+const execute = (script: string): Promise<any> => {
+  return new Promise((resolvePromise, rejectPromise) => {
+    exec(script, (error, stdout, stderr) => {
+      if (error) {
+        rejectPromise(stderr);
+      } else {
+        resolvePromise(stdout);
+      }
+    });
+  });
+};
+
+async function main() {
+  const version = jsonPkg.version;
+
+  console.log('Publishing new version', version);
+
+  await execute(
+    ` cd ${buildPath} && yarn publish  --new-version ${version} --tag latest`
+  );
+}
+
+main();
